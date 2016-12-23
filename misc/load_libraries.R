9f3181e1561a29c79c255f342df8cd24581d368a
jiewpeng <jiewpeng.lim@gmail.com>
2016-12-23 12:42:37 +0800

    Modified to also install libraries if missing

--- misc/load_libraries.R
+++ misc/load_libraries.R
@@ -1,7 +1,20 @@
-library(tidyverse, pos = 2)
-library(rvest)
-library(stringr)
-library(Hmisc)
-library(httr)
-library(rjson)
-library(lubridate)
+packages <- c(
+    "tidyverse", "rvest", "stringr", "Hmisc", "httr", "rjson", "lubridate",
+    "bookdown"
+)
+
+for (package in packages) {
+    if (!require(package, character.only = TRUE, quietly = TRUE)) {
+        install.packages(package, repos = "https://cloud.r-project.org/")
+        if (package == "tidyverse") {
+            library(package, pos = 2, character.only = TRUE)
+        }
+        else {
+            library(package, character.only = TRUE)
+        }
+        
+    }
+}
+
+rm(packages)
+rm(package)
